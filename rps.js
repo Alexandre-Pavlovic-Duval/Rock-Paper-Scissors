7d8aa2f8c3e5109d1b64b4f956ac1d7abcd1335a
Alexandre-Pavlovic-Duval <alexandre.pavlovic.duval@gmail.com>
2024-04-03 10:25:53 +0200

    Modifing the comparaison function

--- rps.js
+++ rps.js
@@ -1,6 +1,6 @@
 const choices = ["Rock", "Paper", "Scissors"];
 
-for (i=0; i < 5; i++) {
+for (let i=0; i < 5; i++) {
     // Computer's choice
     function getComputerChoice(){
         let randomChoices = Math.floor(Math.random() * choices.length);
@@ -14,25 +14,6 @@ for (i=0; i < 5; i++) {
             alert("Wrong choice");
         } else {
             return player_answer;
-        }
-    }
-    
-    // Comparaison Computer vs User
-    function choiceSelection(computerSelection, playerSelection){    
-        if (computerSelection == "scissors" && playerSelection == "rock") {
-            return alert("You Win! Scissors beats Rock");
-        } else if (computerSelection == "paper" && playerSelection == "rock"){
-            return alert("You Lose! Paper beats Rock");
-        } else if (computerSelection == "paper" && playerSelection == "scissors"){
-            return alert("You Win! Scissors beats Paper");
-        } else if (computerSelection == "rock" && playerSelection == "scissors"){
-            return alert("You Lose! Scissors beats Rock");
-        } else if (computerSelection == "rock" && playerSelection == "paper"){
-            return alert("You Win! Paper beats Rock");
-        } else if (computerSelection == "scissors" && playerSelection == "paper"){
-            return alert("You Lose! Scissors beats Paper");
-        } else {
-            return alert("Equality");
         }
     }
 
@@ -54,14 +35,5 @@ for (i=0; i < 5; i++) {
             return "Equality";
         }
     }
-
-    //let game = choiceSelection(getComputerChoice(), player(playerChoice)))
-    //let result = 0
-    //if (game.includes("Win")) {
-    //    result = result += 1;
-    //} else {
-    //    result = result += result;
-    //} 
     console.log(choiceSelection(getComputerChoice(), player(playerChoice)));
-    //choiceSelection(getComputerChoice(), player(playerChoice));
 }
